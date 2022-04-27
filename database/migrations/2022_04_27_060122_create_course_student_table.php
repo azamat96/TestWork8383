0d5d3cb545df178d139f49eaa55ba5e107059c64
azamat96 <azamat.sdu@bk.ru>
2022-04-27 12:14:54 +0600

    Migrations

---
+++ database/migrations/2022_04_27_060122_create_course_student_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_student', function (Blueprint $table) {
+            $table->bigInteger('course_id')->unsigned();
+            $table->bigInteger('student_id')->unsigned();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('grade')->nullable();
+
+            $table->primary(['student_id', 'course_id']);
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_student');
+    }
+}
